f13409d3f909426fb0057fa850ca832aaaeb824e
rohitpatil-webonise <rohit.patil@weboniselab.com>
2023-06-21 00:45:12 +0530

    Initial Commit

---
+++ ExpenseTrackerApp/ExpenseTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerAppApp.swift
+//  ExpenseTrackerApp
+//
+//  Created by Rohit Patil on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
