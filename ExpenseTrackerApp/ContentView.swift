cc041229144cacf67332d60d58ce8c2c91ea8933
rohitpatil-webonise <rohit.patil@weboniselab.com>
2023-06-29 19:21:28 +0530

    Done

--- ExpenseTrackerApp/ContentView.swift
+++ ExpenseTrackerApp/ContentView.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct ContentView: View {
+    @EnvironmentObject var transactionListVM:TransactionListViewModel
+    var demoData:[Double] = [8,2,4,6,12,9,2]
     var body: some View {
         NavigationView{
             ScrollView{
@@ -16,6 +19,28 @@ struct ContentView: View {
                     Text("OverView")
                         .font(.title2)
                         .bold()
+                    
+                    //MARK: Charts
+                    let data = transactionListVM.accumulateTransaction()
+                    if !data.isEmpty{
+                        let totalExpenses = data.last?.1 ?? 0
+                        
+                        CardView {
+                            VStack{
+                                ChartLabel(totalExpenses.formatted(.currency(code: "USD")),type: .title)
+                                LineChart()
+                            }
+                            .background(Color.systemBackground)
+                        }
+                        .data(data)
+                        .chartStyle(ChartStyle(backgroundColor: Color.systemBackground, foregroundColor: ColorGradient(Color.blue.opacity(0.4),Color.blue)))
+                        .frame(height:300)
+                    }
+                    
+                  
+                    
+                    //MARK: TransactionList
+                    RecentTransactionList()
                 }
                 .padding()
                 .frame(maxWidth:.infinity)
@@ -32,15 +57,22 @@ struct ContentView: View {
             }
         }
         .navigationViewStyle(.stack)
+        .accentColor(.primary)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    static let transactionListVM : TransactionListViewModel = {
+        let transactionListVM = TransactionListViewModel()
+        transactionListVM.transactions = transactionListPreviewData
+        return transactionListVM
+    }()
     static var previews: some View {
         Group {
             ContentView()
             ContentView()
                 .preferredColorScheme(.dark)
         }
+        .environmentObject(transactionListVM)
     }
 }
